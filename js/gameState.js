4eeb69f4df5fa251de68e8b2c4d0d19c1cd2cf53
Eric Smith <eric.smith@spidasoftware.com>
2015-08-01 19:06:40 -0400

    Starting

---
+++ js/gameState.js
@@ -0,0 +1,52 @@
+
+var gameState;
+
+function newGame() {
+  gameState = {
+    planets: [
+      new Planet(0, 0, 20903520)
+    ],
+    rocket: Rocket()
+  };
+}
+
+function nearestPlanetDistance() {
+  var idx;
+  var min = -1;
+  for (idx=0; idx < gameState.planets.length; idx ++) {
+    var dist = getDistanceToPlanetSurface(gameState.planets[idx]);
+    if (min == -1 || dist < min) {
+      min = dist;
+    }
+  }
+  return min;
+}
+
+function getDistanceToPlanetSurface(planet) {
+  var xDiff = gameState.rocket.x - planet.x;
+  var yDiff = gameState.rocket.y - planet.y;
+  var distToCenter = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+  return distToCenter - planet.radius;
+}
+
+function Rocket() {
+  var rocket = {
+    x: 0,
+    y: -20903530,
+    dir: 0,
+    velocity: { x: 0, y:-100000 },
+    maxThrust: 100,
+    throttle: 0, //0 to 100
+    move: function(time) {
+      this.x += this.velocity.x * time;
+      this.y += this.velocity.y * time;
+    }
+  };
+  return rocket;
+}
+
+function Planet (x,y,radius) {
+  this.x=x;
+  this.y=y;
+  this.radius=radius;
+}
---
+++ js/rockets.js
@@ -0,0 +1,61 @@
+var ctx;
+var canvas;
+var gameWidth = 800;
+var gameHeight = 800;
+var lastTime;
+var planetPeek = 50;
+
+function start() {
+  canvas = $("#main")[0];
+  newGame();
+  lastTime = new Date().getTime();
+  update();
+}
+
+function update() {
+  ctx = canvas.getContext("2d");
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  var now = new Date().getTime();
+  var timeDiff = now - lastTime;
+  lastTime = now;
+  gameState.rocket.move(timeDiff/1000);
+  drawState();
+  setTimeout(update, 1000/30);
+}
+
+function drawState() {
+  ctx.translate(gameWidth / 2, gameHeight / 2);
+  ctx.rotate(gameState.rocket.dir*Math.PI/180);
+  ctx.beginPath();
+  ctx.moveTo(0, -20);
+  ctx.lineTo(10, 10);
+  ctx.lineTo(-10, 10);
+  ctx.lineTo(0, -20);
+  ctx.stroke();
+  ctx.rotate(gameState.rocket.dir*Math.PI/-180);
+  var scale = (gameHeight / 2 - planetPeek) / nearestPlanetDistance();
+  if (scale > 1) {
+    scale = 1;
+  }
+  var index
+  for (index=0; index < gameState.planets.length; index++) {
+    var planet = gameState.planets[index];
+
+    ctx.translate(scale * (gameState.rocket.x * -1 + planet.x), scale * (gameState.rocket.y * -1 + planet.y));
+    drawCircle(0, 0, planet.radius * scale);
+  }
+}
+
+function drawCircle(x, y, radius) {
+  ctx.beginPath();
+  ctx.arc(x,y,radius,0,2*Math.PI);
+  ctx.stroke();
+}
+
+function fixSize() {
+  $("#main").width($("#mainDiv").width());
+  $("#main").height($("#mainDiv").height);
+}
+
+$(start);
