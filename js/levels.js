2a73888fd982e169be74abb36f3bd0c523b2abe7
Eric Smith <eric.smith@spidasoftware.com>
2017-01-01 14:07:38 -0500

    Count quarters

--- js/levels.js
+++ js/levels.js
@@ -43,7 +43,7 @@ var IntroLevel = function() {
   this.setupPlanetsMoons([new Planet("Minimus", 0, 0, 1000, 1 * Math.pow(10, 17))], []);
   this.rocket.x = 1010;
   this.rocket.y = 0;
-  this.arrivedToLeftHemi = false;
+  this.quartersCompleted = 0; //user needs to complete 4 quarters
   this.travelingClockwise = false;
 };
 IntroLevel.prototype = Object.create(Level.prototype);
@@ -52,20 +52,27 @@ IntroLevel.prototype.start = function() {
 }
 IntroLevel.prototype.goalComplete = function() {
   //planet center is 0,0
-  if (gameState.rocket.x < 0 && !this.arrivedToLeftHemi) {
-    this.arrivedToLeftHemi = true;
-    this.travelingClockwise = gameState.rocket.y < 0;
-  }
-  if (this.arrivedToLeftHemi) {
+  if (this.quartersCompleted == 0 && gameState.rocket.x < 0) {
+    this.quartersCompleted++;
+    this.travelingClockwise = gameState.rocket.y < 0; //y down is negative
+  } else if (this.quartersCompleted == 1) {
+    if (this.travelingClockwise && gameState.rocket.y > 0) {
+      this.quartersCompleted++;
+    } else if (!this.travelingClockwise && gameState.rocket.y < 0) {
+      this.quartersCompleted++;
+    }
+  } else if (this.quartersCompleted == 2) {
     if (gameState.rocket.x > 0) {
-      if (this.travelingClockwise) {
-        return gameState.rocket.y > 0;
-      } else {
-        return gameState.rocket.y < 0;
-      }
+      this.quartersCompleted++;
+    }
+  } else if (this.quartersCompleted == 3) {
+    if (this.travelingClockwise && gameState.rocket.y < 0) {
+      this.quartersCompleted++;
+    } else if (!this.travelingClockwise && gameState.rocket.y > 0) {
+      this.quartersCompleted++;
     }
   }
-  return false;
+  return this.quartersCompleted == 4;
 }
 
 var LowEarthOrbitLevel = function() {
