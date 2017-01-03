6d8edcc3939dc257632cf52cf7d3001cffc9daf2
Eric Smith <eric.smith@spidasoftware.com>
2017-01-02 21:08:59 -0500

    Cleanup

--- js/levels/minimusMoon.js
+++ js/levels/minimusMoon.js
@@ -9,7 +9,6 @@ var MinimusMoon = function() {
   this.rocket.y = 0;
   this.rocket.velocity.y = 0;
   this.rocket.fuel = 14;
-  this.quartersCompleted = 0; //user needs to complete 4 quarters
   this.travelingClockwise = false;
 };
 MinimusMoon.prototype = Object.create(Level.prototype);
@@ -17,26 +16,5 @@ MinimusMoon.prototype.start = function() {
   message("Small planet with moon.");
 }
 MinimusMoon.prototype.goalComplete = function() {
-  //planet center is 0,0
-  if (this.quartersCompleted == 0 && gameState.rocket.x < 0) {
-    this.quartersCompleted++;
-    this.travelingClockwise = gameState.rocket.y < 0; //y down is negative
-  } else if (this.quartersCompleted == 1) {
-    if (this.travelingClockwise && gameState.rocket.y > 0) {
-      this.quartersCompleted++;
-    } else if (!this.travelingClockwise && gameState.rocket.y < 0) {
-      this.quartersCompleted++;
-    }
-  } else if (this.quartersCompleted == 2) {
-    if (gameState.rocket.x > 0) {
-      this.quartersCompleted++;
-    }
-  } else if (this.quartersCompleted == 3) {
-    if (this.travelingClockwise && gameState.rocket.y < 0) {
-      this.quartersCompleted++;
-    } else if (!this.travelingClockwise && gameState.rocket.y > 0) {
-      this.quartersCompleted++;
-    }
-  }
-  return this.quartersCompleted == 4;
+  return false;
 }
